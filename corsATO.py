7668dea5789a6507d2847ea68bb60041f2038027
IHA089 <67224300+Rohitash465@users.noreply.github.com>
2025-03-11 19:15:21 +0530

    Update corsATO.py

--- corsATO.py
+++ corsATO.py
@@ -3,7 +3,7 @@ from flask import Flask, request, make_response, render_template, session, jsoni
 from functools import wraps
 from flask_cors import CORS 
 import jwt as pyjwt
-import uuid, datetime, sqlite3, hashlib, random, os
+import uuid, datetime, sqlite3, hashlib, random, os, string, request
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
@@ -31,7 +31,9 @@ def create_database():
         gmail TEXT NOT NULL,
         username TEXT NOT NULL,
         password TEXT NOT NULL,
-        uuid TEXT NOT NULL
+        uuid TEXT NOT NULL,
+        active TINYINT(1) DEFAULT 0,
+        code TEXT NOT NULL
     )
     ''')
 
@@ -39,11 +41,18 @@ def create_database():
     passw = "admin@"+str(numb)
     passw_hash = hashlib.md5(passw.encode()).hexdigest()
     user_uuid = str(uuid.uuid4())
-    query = "INSERT INTO users (gmail, username, password, uuid) VALUES ('admin@iha089.org', 'admin', '"+passw_hash+"', '"+user_uuid+"')"
+    query = "INSERT INTO users (gmail, username, password, uuid, active, code) VALUES ('admin@iha089.org', 'admin', '"+passw_hash+"', '"+user_uuid+"', '1', '45AEDF32')"
     cursor.execute(query)
     conn.commit()
     conn.close()
 
+def generate_code():
+    first_two = ''.join(random.choices(string.digits, k=2))
+    next_four = ''.join(random.choices(string.ascii_uppercase, k=4))
+    last_two = ''.join(random.choices(string.digits, k=2))
+    code = first_two + next_four + last_two
+    return code
+    
 def check_database():
     db_path = os.path.join(os.getcwd(), lab_type, lab_name, 'users.db')
     if not os.path.isfile(db_path):
@@ -119,6 +128,78 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+@corsATO.route('/confirm', methods=['POST'])
+def confirm():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    code = request.form.get('confirmationcode')
+    hash_password=hashlib.md5(password.encode()).hexdigest()
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT *FROM users WHERE username = ? or gmail = ? AND password=? AND code = ?", (username, username, hash_password, code))
+    user = cursor.fetchone()
+    
+    if user:
+        cursor.execute("UPDATE users SET active = 1 WHERE username = ? or gmail = ?", (username, username))
+        conn.commit()
+        conn.close()
+        session['user'] = username
+        
+        user_uuid = user['uuid'] if 'uuid' in user else str(uuid.uuid4())
+
+        jwt_token = pyjwt.encode({
+            "username": username,
+            "exp": datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)
+        }, JWT_SECRET, algorithm="HS256")
+
+        user_data[user_uuid] = jwt_token
+
+        if 'uuid' not in user:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute("UPDATE users SET uuid = ? WHERE username = ?", (user_uuid, username))
+            conn.commit()
+            conn.close()
+
+        response = make_response(redirect(url_for('dashboard')))
+        response.set_cookie("uuid", user_uuid, httponly=False)
+        response.set_cookie("jwt_token", jwt_token, httponly=True, samesite="Strict")
+        return response
+    
+    conn.close()
+    error_message = "Invalid code"
+    return render_template('confirm.html', error=error_message, username=username, password=password)
+
+@corsATO.route('/resend', methods=['POST'])
+def resend():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    hash_password=hashlib.md5(password.encode()).hexdigest()
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT code FROM users WHERE username = ? or gmail = ? AND password = ?", (username, username, hash_password))
+    code = cursor.fetchone()
+    if code:
+        username=username
+        username = username.replace(" ", "")
+        bdcontent = "<h2>Verify Your Account password</h2><p>your verification code are given below</p><div style=\"background-color: orange; color: black; font-size: 14px; padding: 10px; border-radius: 5px; font-family: Arial, sans-serif;\">"+code[0]+"</div><p>If you did not request this, please ignore this email.</p>"
+        mail_server = "https://127.0.0.1:7089/dcb8df93f8885473ad69681e82c423163edca1b13cf2f4c39c1956b4d32b4275"
+        payload = {"email": username,
+                    "sender":"IHA089 Labs ::: corsATOLab",
+                    "subject":"corsATOLab::Verify Your Accout",
+                    "bodycontent":bdcontent
+                }
+        try:
+            k = requests.post(mail_server, json = payload)
+        except:
+            return jsonify({"error": "Mail server is not responding"}), 500
+        error_message="code sent"
+    else:
+        error_message="Invalid username or password"
+
+    conn.close()
+    return render_template('confirm.html', error=error_message, username=username, password=password)
+    
 @corsATO.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -143,6 +224,8 @@ def login():
     conn.close()
 
     if user:
+        if not user[4] == 1:
+            return render_template('confirm.html', username=username, password=password)
         session['user'] = username
         user_uuid = user['uuid'] if 'uuid' in user else str(uuid.uuid4())
 
@@ -161,7 +244,7 @@ def login():
             conn.close()
 
         response = make_response(redirect(url_for('dashboard')))
-        response.set_cookie("uuid", user_uuid, httponly=False)  # Insecure cookie
+        response.set_cookie("uuid", user_uuid, httponly=False)  
         response.set_cookie("jwt_token", jwt_token, httponly=True, samesite="Strict")
         return response
 
@@ -195,7 +278,7 @@ def protected():
     except pyjwt.InvalidTokenError:
         return jsonify({"message": "Invalid token"}), 401
 
-@corsATO.route('/join', methods=['GET', 'POST'])
+@corsATO.route('/join', methods=['POST'])
 def join():
     if not check_cookies():
         session.clear()
@@ -211,7 +294,7 @@ def join():
     conn = get_db_connection()
     cursor = conn.cursor()
     hash_password = hashlib.md5(password.encode()).hexdigest()
-    query = f"INSERT INTO users (gmail, username, password) VALUES ('{email}', '{username}', '{hash_password}')".format(email, username, password)
+    
     cursor.execute("SELECT * FROM users where gmail = ?", (email,))
     if cursor.fetchone():
         error_message = "Email already taken. Please choose another."
@@ -220,14 +303,24 @@ def join():
     
     try:
         user_uuid = str(uuid.uuid4())
-
-        cursor.execute("INSERT INTO users (gmail, username, password, uuid) VALUES (?, ?, ?, ?)", (email, username, hash_password, user_uuid))
+        code = generate_code()
+        cursor.execute("INSERT INTO users (gmail, username, password, uuid, active, code) VALUES (?, ?, ?, ?, ?, ?)", (email, username, hash_password, user_uuid, '0', code))
         conn.commit()
+        username=email
+        username = username.replace(" ", "")
+        bdcontent = "<h2>Verify Your Account password</h2><p>your verification code are given below</p><div style=\"background-color: orange; color: black; font-size: 14px; padding: 10px; border-radius: 5px; font-family: Arial, sans-serif;\">"+code+"</div><p>If you did not request this, please ignore this email.</p>"
+        mail_server = "https://127.0.0.1:7089/dcb8df93f8885473ad69681e82c423163edca1b13cf2f4c39c1956b4d32b4275"
+        payload = {"email": username,
+                    "sender":"IHA089 Labs ::: corsATOLab",
+                    "subject":"corsATOLab::Verify Your Accout",
+                    "bodycontent":bdcontent
+                }
+        try:
+            k = requests.post(mail_server, json = payload)
+        except:
+            return jsonify({"error": "Mail server is not responding"}), 500
 
-        response = make_response(render_template('login.html'))
-        response.set_cookie("uuid", user_uuid, httponly=False)  # Insecure cookie
-        return response
-
+        return render_template('confirm.html', username=email, password=password)
     except sqlite3.Error:
         error_message = "Something went wrong. Please try again later."
         return render_template('join.html', error=error_message)
@@ -243,9 +336,6 @@ def dashboard():
         session.clear()
     if 'user' not in session:
         return redirect(url_for('login_html'))
-    admin_list=['admin', 'administrator']
-    if session.get('user') in admin_list:
-        return render_template('admin-dashboard.html', user=session.get('user'))
 
     return render_template('dashboard.html', user=session.get('user'))
 
@@ -253,7 +343,7 @@ def dashboard():
 def logout():
     session.clear() 
     response = make_response(redirect(url_for('login_html')))
-    response.set_cookie("uuid", "", httponly=False)  # Insecure cookie
+    response.set_cookie("uuid", "", httponly=False)  
     response.set_cookie("jwt_token", "", httponly=True, samesite="Strict")
     return response
 
